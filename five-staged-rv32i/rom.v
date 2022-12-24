363cf2bae244506a6ee9440e7b091cff6d8ec5de
Yuto Nakamura <maron07222010@gmail.com>
2022-12-24 13:35:20 +0900

    fix

--- five-staged-rv32i/rom.v
+++ five-staged-rv32i/rom.v
@@ -3,7 +3,7 @@ module rom(input clk,
              output reg [31:0] r_data
             );
   //TODO: initial文でデータを読み込む
-  reg [31:0] mem[0:'h1a40]; //0x100+0x6000を32bitで割った値
+  reg [31:0] mem[0:'h1a40];
   initial
  $readmemh("/path/to/coremark/code.hex", mem);
   wire [31:0] addr;
